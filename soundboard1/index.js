a80e795f71e9f37c566ab248083c79b2f9dcd563
Nick <barnard.nicholas@googlemail.com>
2019-12-17 13:54:36 +0000

    added quickfire & looping sounds

--- soundboard1/index.js
+++ soundboard1/index.js
@@ -1,7 +1,12 @@
 const sound1Button = document.getElementById("sound1button");
 const stopButton = document.getElementById("stopbutton");
+const qfButton = document.getElementById("qfbutton");
+const loopButton = document.getElementById("loopbutton");
+const stopLoopButton = document.getElementById("stoploopbutton");
 
 const sound1 = new Howl({ src: ["audio/tyunwin.mp3"] });
+const sound2 = new Howl({ src: ["audio/jump.wav"] });
+const sound3 = new Howl({ src: ["audio/genloop.wav"], loop: true });
 
 const soundRef = {
   sound1button: sound1
@@ -31,8 +36,41 @@ const button1Clicked = clickEvent => {
 const stopButtonClicked = clickEvent => {
   console.log("Stopping all audio...");
   sound1Button.className = "stopped";
-  console.dir(sound1Button);
   sound1.stop();
+};
+
+const qfButtonClicked = clickEvent => {
+  console.log("Playing Quickfire Sound...");
+  qfButton.className = "playing";
+  sound2.play();
+  sound2.once("end", () => {
+    qfButton.className = "stopped";
+  });
+};
+
+const loopButtonClicked = clickEvent => {
+  let clickedItemClass = clickEvent.target.className;
+  const clickedItemId = clickEvent.target.attributes[0].value;
+  if (clickedItemClass === "stopped") {
+    console.log(`Playing looping sound for ${clickedItemId}...`);
+    clickEvent.target.className = "playing";
+    sound3.play();
+    console.dir(clickEvent.target);
+  } else if (clickedItemClass === "playing") {
+    console.log(`Pausing looping sound for ${clickedItemId}...`);
+    clickEvent.target.className = "paused";
+    sound3.pause();
+  } else if (clickedItemClass === "paused") {
+    console.log(`Unpausing looping sound for ${clickedItemId}...`);
+    clickEvent.target.className = "playing";
+    sound3.play();
+  }
+};
+
+const stopLoopButtonClicked = clickEvent => {
+  console.log("Stopping Sound 1...");
+  loopButton.className = "stopped";
+  sound3.stop();
 };
 
 // buttons.forEach(button => {
@@ -41,3 +79,6 @@ const stopButtonClicked = clickEvent => {
 
 sound1Button.addEventListener("click", button1Clicked);
 stopButton.addEventListener("click", stopButtonClicked);
+qfButton.addEventListener("click", qfButtonClicked);
+loopButton.addEventListener("click", loopButtonClicked);
+stopLoopButton.addEventListener("click", stopLoopButtonClicked);
