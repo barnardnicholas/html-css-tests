ae5e95cebb7d264c73e4cb28303e86a3718920a9
Nick <barnard.nicholas@googlemail.com>
2019-12-18 22:01:05 +0000

    new sounds, mute buttons, readouts for UI

--- nature-sound-gen/sound-gen.js
+++ nature-sound-gen/sound-gen.js
@@ -1,44 +1,48 @@
 const startButton = document.getElementById("soundscapestart");
 const stopButton = document.getElementById("stopbutton");
+const muteBackground = document.getElementById("mutebg");
+const muteRandom = document.getElementById("muterandom");
+const backgroundReadout = document.getElementById("bgreadout");
+const randomReadout = document.getElementById("randomreadout");
 
 const bgLoop = new Howl({
-  src: ["audio/loops/forest_loop1.wav"],
+  src: ["audio/loops/jungle2.ogg"],
   loop: true,
   html5: false
 });
 console.dir(bgLoop.volume);
 const sound1 = new Howl({
-  src: ["audio/bird_calls/bird001.wav"],
+  src: ["audio/bird_calls/Junglebird1.ogg"],
   html5: false,
   volume: 0.5
 });
 const sound2 = new Howl({
-  src: ["audio/bird_calls/coot.wav"],
+  src: ["audio/bird_calls/Junglebird2.ogg"],
   html5: false,
   volume: 0.5
 });
 const sound3 = new Howl({
-  src: ["audio/bird_calls/crow.wav"],
+  src: ["audio/bird_calls/Junglebird3.ogg"],
   html5: false,
   volume: 0.5
 });
 const sound4 = new Howl({
-  src: ["audio/bird_calls/robin.wav"],
+  src: ["audio/bird_calls/Junglebird4.ogg"],
   html5: false,
   volume: 0.5
 });
 const sound5 = new Howl({
-  src: ["audio/bird_calls/rooster.wav"],
+  src: ["audio/bird_calls/Junglebird5.ogg"],
   html5: false,
   volume: 0.5
 });
 const sound6 = new Howl({
-  src: ["audio/bird_calls/tawny_owl.wav"],
+  src: ["audio/bird_calls/Junglebird6.ogg"],
   html5: false,
   volume: 0.5
 });
 const sound7 = new Howl({
-  src: ["audio/bird_calls/yellow_wattlebird.wav"],
+  src: ["audio/bird_calls/Junglebird7.ogg"],
   html5: false,
   volume: 0.5
 });
@@ -89,10 +93,11 @@ function playSound(delay) {
   }
   const thisSound = soundQueue.shift();
   const panAmount = Math.random() * 2 - 1;
-  const volumeAmount = Math.random() * 0.5;
+  const volumeAmount = Math.random();
   thisSound.volume(volumeAmount);
   // thisSound.stereo(panAmount);
   thisSound.play();
+  randomReadout.innerText = `Last-played sound: ${thisSound._src}`;
   console.log(
     `Sound: ${thisSound._src}     Delay: ${delay}ms     Volume: ${volumeAmount}     Pan: ${panAmount}    Last played: ${lastPlayed}`
   );
@@ -117,7 +122,16 @@ startButton.addEventListener("click", clickEvent => {
     console.log("Starting Soundscape...");
     soundscapeRunning = true;
     clickEvent.target.className = "playing";
+    Howler.volume(1);
+    bgLoop.mute(false);
+    muteBackground.className = "unmuted";
+    allSounds.forEach(sound => {
+      sound.mute(false);
+    });
+    muteRandom.className = "unmuted";
+    console.log(`Background Sound: ${bgLoop._src}`);
     bgLoop.play();
+    backgroundReadout.innerText = `Background sound: ${bgLoop._src}`;
     loop();
   } else if (soundscapeRunning === true) {
     console.log("Soundscape already running.");
@@ -128,7 +142,41 @@ startButton.addEventListener("click", clickEvent => {
 stopButton.addEventListener("click", clickEvent => {
   console.log("Stopping soundscape...");
   startButton.className = "stopped";
-  Howler.mute = true;
+  muteBackground.className = "unmuted";
+  muteRandom.className = "unmuted";
   soundscapeRunning = false;
+  Howler.volume(0);
   bgLoop.stop();
+  backgroundReadout.innerText = "Background sound: -";
+  randomReadout.innerText = "Last-played sound: -";
+});
+
+muteBackground.addEventListener("click", clickEvent => {
+  if (clickEvent.target.className === "unmuted") {
+    console.log("Muting Background sounds...");
+    clickEvent.target.className = "muted";
+    bgLoop.mute(true);
+    console.dir(clickEvent.target);
+  } else if (clickEvent.target.className === "muted") {
+    console.log("Un-muting Background sounds...");
+    clickEvent.target.className = "unmuted";
+    bgLoop.mute(false);
+    console.dir(clickEvent.target);
+  }
+});
+
+muteRandom.addEventListener("click", clickEvent => {
+  if (clickEvent.target.className === "unmuted") {
+    console.log("Muting Random sounds...");
+    clickEvent.target.className = "muted";
+    allSounds.forEach(sound => {
+      sound.mute(true);
+    });
+  } else if (clickEvent.target.className === "muted") {
+    console.log("Un-muting Random sounds...");
+    clickEvent.target.className = "unmuted";
+    allSounds.forEach(sound => {
+      sound.mute(false);
+    });
+  }
 });
